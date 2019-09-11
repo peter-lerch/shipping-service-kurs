6c506d9c133754beeeab31ad747b24929c7d48ad
Peter Lerch <peter.lerch@ch.ibm.com>
2019-09-11 15:33:35 +0200

    Lab 2

--- tests/product-service.test.js
+++ tests/product-service.test.js
@@ -6,7 +6,7 @@ var nock = require('nock')
 
 describe('Product service', function () {
   it('Should call remote service', async function () {
-    nock('https://product.service:8899/products')
+    nock('https://mycluster.icp:8899/products')
       .get('/13')
       .delayBody(10)
       .reply(200, {
@@ -19,7 +19,7 @@ describe('Product service', function () {
   })
 
   it('Should handle unexpected response structure', async function () {
-    nock('https://product.service:8899/products')
+    nock('https://mycluster.icp:8899/products')
       .get('/19')
       .reply(200, {
         res: 15.5
@@ -28,10 +28,10 @@ describe('Product service', function () {
     await productService
       .getProductWeight('19')
       .then(() => {
-        throw (new Error('Should not resolve in case of malformed data'))
+        throw(new Error('Should not resolve in case of malformed data'))
       })
       .catch(err => {
-        expect(err.message).toBe('Invalid response object');
+        expect(err.message).toBe('Invalid response object')
       })
   })
 })
